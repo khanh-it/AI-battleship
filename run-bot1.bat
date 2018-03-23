214a90064958e074e0a58366e6f9186906c2a30d
tinpro238 <thanhtin238@gmail.com>
2018-03-23 09:34:17 +0700

    Customize config bot

--- run-bot1.bat
+++ run-bot1.bat
@@ -1,4 +1,4 @@
 @echo off
-SET AI_NAME=bot1
-explorer "http://10.11.8.92:8001/game-engine.php"
-.\php\php -c .\php\ -S 10.11.8.92:8001 -t .\www\public_html
+SET AI_NAME=bot_tin1
+explorer "http://10.11.8.157:8001/game-engine.php"
+.\php\php -c .\php\ -S 10.11.8.157:8001 -t .\www\public_html
--- run-bot2.bat
+++ run-bot2.bat
@@ -1,4 +1,4 @@
 @echo off
-SET AI_NAME=bot2
-explorer "http://10.11.8.92:8002/game-engine.php"
-.\php\php -c .\php\ -S 10.11.8.92:8002 -t .\www\public_html
+SET AI_NAME=bot_tin2
+explorer "http://10.11.8.157:8002/game-engine.php"
+.\php\php -c .\php\ -S 10.11.8.157:8002 -t .\www\public_html
